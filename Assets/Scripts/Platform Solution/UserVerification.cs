83b8fd3b46890253354b0181ee7b1338754530bd
danrayfr <danrayrollan98@gmail.com>
2022-12-04 01:04:26 +0800

    Add App API

---
+++ Assets/Scripts/Platform Solution/UserVerification.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Platform.Models;
+
+public class UserVerification : MonoBehaviour {
+    void Start() { 
+
+    }
+
+    void Update() { 
+
+    }
+
+    
+}
